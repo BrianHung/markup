3f1ad67a0733bd6be0835e2bb5a6cff636688116
Brian Hung <brianhung@berkeley.edu>
2018-12-16 14:58:27 -0800

    initial commit

---
+++ markup/markup.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    chrome.storage.sync.get(['value'], function(query) {
+        // Notify that we saved.
+        document.getElementById("editor").innerHTML = query.value;
+    });
+
+    document.getElementById("editor").addEventListener("input", function() {
+
+        // Get a value saved in a form.
+        var textValue = document.getElementById("editor").innerHTML;
+
+        chrome.extension.getBackgroundPage().console.log('markup.js');
+
+        // Save it using the Chrome extension storage API.
+        chrome.storage.sync.set({'value': textValue}, function() {
+            
+            // Notify that we saved.
+            console.log("edits have been saved!");
+            chrome.storage.sync.get(function(result){console.log(result)})
+        });
+
+    });
+
+
+});
