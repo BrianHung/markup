5aecd7d335fa38852623d69b06ac9db3cff76a08
Brian Hung <brianhung@berkeley.edu>
2018-12-23 15:21:45 -0800

    collapsible headers done!

---
+++ markup/collapse.js
@@ -0,0 +1,24 @@
+var headTags = ["H1", "H2", "H3", "H4", "H5", "H6"];
+document.querySelector('body').addEventListener('click', function(event) {
+    var targetTag = event.target.tagName;
+    if (headTags.includes(targetTag)) {
+
+        console.log(targetTag);
+        var higherTags = headTags.slice(0, headTags.indexOf(targetTag) + 1)
+
+        var currentElement = event.target.nextElementSibling;
+        if (currentElement) {
+
+            var displayStyle = "block";
+            if (currentElement.style.display == "block") {
+                displayStyle = "none";
+            }
+
+        	while (currentElement && !higherTags.includes(currentElement.tagName)) {
+                currentElement.style.display = displayStyle;
+                currentElement = currentElement.nextElementSibling;
+            }
+        }
+
+    }
+});
