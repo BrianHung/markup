72e279f6fe615f8e64f222ad2ec704e8a7efb2ef
Brian Hung <brianhung@berkeley.edu>
2018-12-23 17:08:52 -0800

    finishing collapsible lists..

--- markup/collapse.js
+++ markup/collapse.js
@@ -1,7 +1,10 @@
 var headTags = ["H1", "H2", "H3", "H4", "H5", "H6"];
 document.querySelector('body').addEventListener('click', function(event) {
     var targetTag = event.target.tagName;
-    if (headTags.includes(targetTag)) {
+
+    console.log(event.offsetX, event.target.offsetWidth);
+
+    if (event.offsetX < 42 && headTags.includes(targetTag)) {
 
         console.log(targetTag);
         var higherTags = headTags.slice(0, headTags.indexOf(targetTag) + 1)
